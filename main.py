a3a585b2c0fcd6a83b88e7c45171eef0c14a6795
Suleyman Farajli <suleyman@farajli.net>
2025-07-19 14:12:31 +0400

    init: first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+def preprocess_mail(raw_mail):
+    # Preprocess raw mail string
+    pass
+
+def parse_mail(mail):
+    # Parse preprocessed mail string
+    pass
+
+def train():
+    # Train the model
+    pass
+
+def predict():
+    # Predict whether it is spam or not from the input mail
+    pass
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
